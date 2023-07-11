e66111c4d8ccf2fb4dbbda8ff582f2706532114a
Gabriel Rockson <gabriel.rockson@amalitech.com>
2023-07-11 19:56:49 +0000

    feat: Remove things and add things

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ function App() {
     <>
       <h1>Welcome to our fresh application</h1>
       <h2>Gabriel came in here</h2>
+      <h3>
+        Essel asked me to add this line, blame him. I have also removed the old
+        stuff.
+      </h3>
     </>
   );
 }
