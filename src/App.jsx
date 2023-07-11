6295105cd109e73990763906b5b4c2f3c1d2f330
Gabriel Rockson <gabriel.rockson@amalitech.com>
2023-07-11 19:34:31 +0000

    feat: Add chilling messages

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ function App() {
     <>
       <h1>Welcome to our fresh application</h1>
       <h2>Gabriel came in here</h2>
+      <h2>Rockson is in here, chilling.</h2>
+      <h3>Rockson doesn't owe anybody anything.</h3>
+      <h3>Rockson is enjoying life in the code</h3>
     </>
   );
 }
